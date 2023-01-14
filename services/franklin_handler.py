4d46e5697df9c1a4bf5fc0b9424ae2c2a5e83202
Jonah Grossberg <jonahag@gmail.com>
2023-01-14 15:24:39 -0500

    update the prompt to use the highlight tags

--- services/franklin_handler.py
+++ services/franklin_handler.py
@@ -1,36 +1,37 @@
 import os, logging
 import openai
 
+
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def handle(request):
     try:
+
+        print("Request inputs: {}".format(request.form))
+        print("Prompt: {}".format(generate_prompt(request.form)))
+        
         response = openai.Completion.create(
             model="text-davinci-002",
             prompt=generate_prompt(request.form),
             max_tokens=100,
-            temperature=0.6,
+            temperature=0.5,
         )
+        print("Response: {}".format(response))
         return response.choices[0].text
     except:
         logging.exception("")
+
         print("failed")
 
 
 def generate_prompt(form):
-    return """Write a passionate real estate MLS listing description for a {} bedroom {} bathroom house in {}""".format(
-        form["bedroom-count"], form["bathroom-count"], form["zipcode"]
+    if len(form["highlights"]) > 1:
+        return """Write a passionate real estate MLS listing description for a {} bedroom {} bathroom house in {}. In the description, try to emphasize the house's {} and location (look up the city from the zip code).""".format(
+            form["bedroom-count"], form["bathroom-count"], form["zipcode"], form["highlights"]
     )
 
+    return """Write a passionate real estate MLS listing description for a {} bedroom {} bathroom house in {}.""".format(
+            form["bedroom-count"], form["bathroom-count"], form["zipcode"])
 
-class ChatGPT:
-    def create_completion(self, prompt):
-        try:
-            response = openai.Completion.create(
-                model="text-davinci-002", prompt=prompt, max_tokens=10, temperature=0.6
-            )
-            return response.choices[0].text
-        except:
-            logging.exception("")
-            print("failed")
